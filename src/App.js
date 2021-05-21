4d4a7daba63451fd894717bb50eee5b72ab68d72
Company <3162913219@qq.com>
2021-05-21 10:17:03 +0800

    draw at canvas

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import {defineComponent,h} from '@vue/runtime-core'
 export default defineComponent({
     render(){
         // 创建 vnode
-        const vnode = h('div')
-        console.log(vnode)
+        // const vnode = h('rect',{x:100,y:100},"我的头发是真的！") 等价于下面
+        // <rect x=100 y=100>我的头发是真的！<rect>
+        const vnode = h('rect',{x:100,y:100},[
+            "我的头发是真的！",
+            h("circle",{x:150,y:150})
+        ])
         return vnode
     }
 })
--- src/runtime-canvas/index.js
+++ src/runtime-canvas/index.js
@@ -2,10 +2,33 @@ import { createRenderer } from '@vue/runtime-core'
 
 const renderer = createRenderer({
     createElement(type){
-
+        // 绘制一个矩形
+        let element
+        if(type== 'rect'){
+            element = new Graphics()
+            element.beginFill('0xff0000')
+            element.drawRect(0,0,500,500)
+            element.endFill()
+        }else if(type== 'circle'){
+            element = new Graphics()
+            element.beginFill('0x00ff00')
+            element.drawCircle(0,0,50)
+            element.endFill()
+        }
+        return element
+    },
+    setElementText(node,text){
+        const cText = new Text(text)
+        node.addChild(cText)
+    },
+    createText(text){
+        return new Text(text)
+    },
+    patchProp(el,key,preValue,nextValue){
+        el[key] = nextValue
     },
     insert(el,parent){
-        
+        parent.addChild(el)
     }
 })
 
